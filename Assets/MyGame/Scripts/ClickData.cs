16f94a8b4a3a282b80730202f96640d503ff064e
pkikl <peter.kikl02@htl-salzburg.ac.at>
2021-06-14 13:17:57 +0200

    added scriptable Objects excercise

---
+++ Assets/MyGame/Scripts/ClickData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "ClickData")]
+public class ClickData : ScriptableObject, ISerializationCallbackReceiver
+{
+    public int clickInfoInitial;
+    public int clickInfoRuntime;
+
+    public void OnAfterDeserialize()
+    {
+        clickInfoRuntime = clickInfoInitial;
+
+        if(clickInfoRuntime >= 100)
+        {
+            clickInfoRuntime = 0;
+        }
+    }
+
+    public void OnBeforeSerialize()
+    { 
+
+    }
+
+}
+
+
